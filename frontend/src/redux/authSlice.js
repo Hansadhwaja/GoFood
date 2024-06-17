d72c93a0a664c67d923297b636350e0626ee8d37
Hansadhwaja Biswal <hansadhwajabiswal@gmail.com>
2024-06-17 10:08:07 +0530

    updated authSlice

--- frontend/src/redux/authSlice.js
+++ frontend/src/redux/authSlice.js
@@ -1,15 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = false; // User is logged out by default
+const initialState = JSON.parse(localStorage.getItem('auth')) || false; // Read from localStorage
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
     login: (state) => {
+      localStorage.setItem('auth', true); // Save to localStorage
       return true; // User is logged in
     },
     logout: (state) => {
+      localStorage.setItem('auth', false); // Save to localStorage
       return false; // User is logged out
     }
   }
